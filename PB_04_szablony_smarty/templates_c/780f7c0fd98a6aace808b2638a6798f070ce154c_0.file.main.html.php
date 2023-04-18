be493e9309e0ed0e07145779545b9900193ded77
PiotrBlachlinski <116668375+PiotrBlachlinski@users.noreply.github.com>
2023-04-18 11:14:12 +0200

    Add files via upload

---
+++ PB_04_szablony_smarty/templates_c/780f7c0fd98a6aace808b2638a6798f070ce154c_0.file.main.html.php
@@ -0,0 +1,160 @@
+<?php
+/* Smarty version 4.1.1, created on 2023-04-17 14:14:30
+  from 'D:\Xampp\htdocs\PB_04_szablony_smarty\templates\main.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.1.1',
+  'unifunc' => 'content_643d38266517e2_13863098',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '780f7c0fd98a6aace808b2638a6798f070ce154c' => 
+    array (
+      0 => 'D:\\Xampp\\htdocs\\PB_04_szablony_smarty\\templates\\main.html',
+      1 => 1680588965,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_643d38266517e2_13863098 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo (($tmp = $_smarty_tpl->tpl_vars['page_description']->value ?? null)===null||$tmp==='' ? "Opis domyślny" ?? null : $tmp);?>
+">
+
+    <title><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_title']->value ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</title>
+
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure.css">
+
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/main-grid-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/main-grid.css">
+    <!--<![endif]-->
+  
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/layouts/marketing-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/layouts/marketing.css">
+    <!--<![endif]-->
+
+    <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/style.css">
+<?php if ($_smarty_tpl->tpl_vars['hide_intro']->value) {?>
+    <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/style_hide_intro.css">
+<?php }?>
+	
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+	<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/js/jquery.min.js"><?php echo '</script'; ?>
+>
+	<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/js/softscroll.js"><?php echo '</script'; ?>
+>
+
+</head>
+<body>
+
+<div id="app_top" class="header">
+    <div class="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href=""><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_title']->value ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</a>
+        <ul>
+            <li class="pure-menu-selected"><a href="#app_top">Góra strony</a></li>
+            <li><a href="#app_content">Przejdź do kalkulatora</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="splash-container">
+    <div class="splash">
+        <h1 class="splash-head"><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_title']->value ?? null)===null||$tmp==='' ? "Domyślny tytuł strony" ?? null : $tmp);?>
+</h1>
+        <p class="splash-subhead">
+             <?php echo (($tmp = $_smarty_tpl->tpl_vars['page_description']->value ?? null)===null||$tmp==='' ? "Domyślny opis strony" ?? null : $tmp);?>
+
+        </p>
+        <p>
+            <a href="#app_content" class="pure-button pure-button-primary">Idź do formularza</a>
+        </p>
+    </div>
+</div>
+
+<div class="content-wrapper">
+
+    <div id="app_content" class="content">
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_783157326643d3826648fc8_18578217', 'content');
+?>
+
+
+    </div>
+
+    <div class="footer l-box is-center">
+		<p>
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1711478382643d3826650dd6_01198364', 'footer');
+?>
+
+		</p>
+        <p>Widok oparty na stylach i szablonie <a href="http://purecss.io/" target="_blank">Pure CSS Yahoo!</a>. (autor przykładu orygianlnego: Przemysław Kudłacik). Zmienione na potrzeby zajęć przez Piotr.B</p>
+    </div>
+
+</div>
+
+
+</body>
+</html><?php }
+/* {block 'content'} */
+class Block_783157326643d3826648fc8_18578217 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_783157326643d3826648fc8_18578217',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+/* {block 'footer'} */
+class Block_1711478382643d3826650dd6_01198364 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'footer' => 
+  array (
+    0 => 'Block_1711478382643d3826650dd6_01198364',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść stopki .... <?php
+}
+}
+/* {/block 'footer'} */
+}
